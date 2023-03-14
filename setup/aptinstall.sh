189666863f11d99cd871f4df29a78828680f335d
David Rambo <davidrambo@mailfence.com>
2023-03-13 21:15:00 -0700

    Add required packages for dev

--- setup/aptinstall.sh
+++ setup/aptinstall.sh
@@ -14,15 +14,31 @@ function install {
     fi
 }
 
-
+install build-essential
+install curl
 install font-manager
 install fonts-font-awesome
 install fzf
 install htop
+install libffi-dev 
+install liblzma-dev
+install libncursesw5-dev 
+install libreadline-dev 
+install libsqlite3-dev 
+install libssl-dev 
+install libbz2-dev 
+install libxml2-dev 
+install libxmlsec1-dev
+install llvm
+install make
 install python3-pip
 install python3-venv
+install tk-dev 
 install tree
 install vim
+install wget
+install xz-utils 
+install zlib1g-dev
 install zsh
 install zsh-syntax-highlighting
 install zsh-autosuggestions
