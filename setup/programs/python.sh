cce33841f0dc31578eec509b585c29d4be847d46
David Rambo <davidrambo@mailfence.com>
2023-03-13 21:15:14 -0700

    Add hatch completions, pyenv version install

--- setup/programs/python.sh
+++ setup/programs/python.sh
@@ -19,6 +19,7 @@ if check_installed pipx; then
 else
     python3 -m pip install --user pipx
     python3 -m pipx ensurepath
+    source $XDG_CONFIG_HOME/zsh/.zshrc
 fi
 
 # pyenv
@@ -26,4 +27,16 @@ if check_installed pyenv; then
     echo "Already installed: pyenv"
 else
     curl https://pyenv.run | bash
+
+    pyenv install -v 3.11.1
+    pyenv global 3.11.1
 fi
+
+# hatch
+if check_installed hatch; then
+    echo "Already installed: hatch"
+else
+    pipx install hatch
+    _HATCH_COMPLETE=zsh_source hatch > $ZDOTDIR/.hatch-complete.zsh
+fi
+
