bedbe4ca44f8ef0e05fd6e3ab9d7a57f048de6f7
David Rambo <davidrambo@mailfence.com>
2023-03-13 20:31:07 -0700

    Setup dotfiles and neovim

--- setup/dotfiles.sh
+++ setup/dotfiles.sh
@@ -1,18 +1,36 @@
 #!/bin/bash
 # Sets up dotfiles from a bare git repo
 
-alias dots='/usr/bin/git --git-dir=$HOME/repos/dotfiles/' --work-tree=$HOME'
+alias dots='/usr/bin/git --git-dir=/home/david/repos/dotfiles/ --work-tree=/home/david'
 
-mkdir -p ~/repos/dotfiles/
+if [ -e /home/david/repos/dotfiles/.gitignore ]; then
+	echo "dotfiles already cloned"
+else
+	mkdir -p /home/david/repos/dotfiles/
 
-git clone --bare https://github.com/DavidRambo/dotfiles.git
+	git clone --bare https://github.com/DavidRambo/dotfiles.git /home/david/repos/dotfiles/
 
-# From atlassian.com/git/tutorials/dotfiles
+	# From atlassian.com/git/tutorials/dotfiles
+	mkdir -p .config-backup && \
+       dots checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | \
+       xargs -I{} mv {} .config-backup/{}
 
-mkdir -p .config-backup && \
-config checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | \
-xargs -I{} mv {} .config-backup/{}
+	dots checkout
 
-dots checkout
+	dots config --local status.showUntrackedFiles no
+fi
 
-dots config --local status.showUntrackedFiles no
+# Neovim config
+
+if [ -e /home/david/.config/nvim/.gitignore ]; then
+	echo "Neovim config already cloned"
+else
+	mkdir -p ~/.config/nvim
+
+	git clone https://github.com/DavidRambo/neovim_config.git ~/.config/nvim/
+
+    # Setup Packer b/c bootstrapping wont' work
+    git clone --depth 1 https://github.com/wbthomason/packer.nvim\
+     ~/.local/share/nvim/site/pack/packer/start/packer.nvim
+fi
+
