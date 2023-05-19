63bb09deb61345bc69e6e7e053ae946684327001
David Rambo <davidrambo@mailfence.com>
2023-05-19 10:00:24 -0700

    Add line for clock offset (commented)

--- undervolt.sh
+++ undervolt.sh
@@ -10,3 +10,6 @@ nvidia-smi -i 0 -pl 225
 
 # limit clock speeds to min 200 and max 1785
 nvidia-smi -i 0 -lgc 200,1785
+
+# frequency offset
+# nvidia-settings -a '[gpu:0]/GPUGraphicsClockOffset[4]=100' -a '[gpu:0]/GPUGraphicsClockOffset[3]=100' -a '[gpu:0]/GPUGraphicsClockOffset[2]=100'
