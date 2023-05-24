e4cdd0b9a14e9d214d52c54ef49974bc8ed3c74f
David Rambo <davidrambo@mailfence.com>
2023-05-24 09:45:08 -0700

    Split off pop shell's gsettings

---
+++ setup/pop_shell_gsettings.sh
@@ -0,0 +1,17 @@
+# Pop Shell
+gsettings set org.gnome.shell.extensions.pop-shell active-hint "true"
+gsettings set org.gnome.shell.extensions.pop-shell active-hint-border-radius "uint32 2"
+gsettings set org.gnome.shell.extensions.pop-shell gap-inner "uint32 3"
+gsettings set org.gnome.shell.extensions.pop-shell gap-outer "uint32 3"
+gsettings set org.gnome.shell.extensions.pop-shell hint-color-rgba "'rgb(110,115,141)'"
+
+gsettings set org.gnome.shell.extensions.pop-shell focus-down "['<Super>Down', '<Super>KP_Down', '<Super>e']"
+gsettings set org.gnome.shell.extensions.pop-shell focus-left "['<Super>Left', '<Super>KP_Left', '<Super>n']"
+gsettings set org.gnome.shell.extensions.pop-shell focus-right "['<Super>Right', '<Super>KP_Right', '<Super>i']"
+gsettings set org.gnome.shell.extensions.pop-shell focus-up "['<Super>Up', '<Super>KP_Up', '<Super>u']"
+
+gsettings set org.gnome.shell.extensions.pop-shell tile-move-down-global "['<Shift><Super>e']"
+gsettings set org.gnome.shell.extensions.pop-shell tile-move-left-global "['<Shift><Super>n']"
+gsettings set org.gnome.shell.extensions.pop-shell tile-move-right-global "['<Shift><Super>i']"
+gsettings set org.gnome.shell.extensions.pop-shell tile-move-up-global "['<Shift><Super>u']"
+gsettings set org.gnome.shell.extensions.pop-shell tile-orientation "['<Super>o']"
