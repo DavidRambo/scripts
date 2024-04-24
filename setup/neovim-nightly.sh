29a121459b68e0b02472a961b6cdcf2fffc4d983
David Rambo <davidrambo@mailfence.com>
2024-04-23 21:41:50 -0700

    rm build when remaking

--- setup/neovim-nightly.sh
+++ setup/neovim-nightly.sh
@@ -9,12 +9,11 @@ if [ ! -d ~/repos/neovim/ ]; then
     git clone https://github.com/neovim/neovim.git
 else
     cd ~/repos/neovim/
+    echo -e ">>> Pulling neovim repo..."
     git pull
 fi
 
-cd ~/repos/neovim/
-
-make clean
+cd ~/repos/neovim/ && rm -r build
 
 make CMAKE_BUILD_TYPE=RelWithDebInfo CMAKE_EXTRA_FLAGS="-DCMAKE_INSTALL_PREFIX=$HOME/.local/neovim"
 
