4e61802b6611adc88ae660c8a831da0ecc9ea776
David Rambo <davidrambo@mailfence.com>
2024-04-24 08:27:41 -0700

    Add some echo messages

--- setup/neovim-nightly.sh
+++ setup/neovim-nightly.sh
@@ -6,6 +6,7 @@ fi
 
 if [ ! -d ~/repos/neovim/ ]; then
     cd ~/repos
+    echo -e ">>> Cloning neovim repo..."
     git clone https://github.com/neovim/neovim.git
 else
     cd ~/repos/neovim/
@@ -13,8 +14,11 @@ else
     git pull
 fi
 
+echo -e ">>> Removing build directory..."
 cd ~/repos/neovim/ && rm -r build
 
+echo -e ">>> Building neovim..."
 make CMAKE_BUILD_TYPE=RelWithDebInfo CMAKE_EXTRA_FLAGS="-DCMAKE_INSTALL_PREFIX=$HOME/.local/neovim"
 
+echo -e ">>> Installing neovim..."
 make install
